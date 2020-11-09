34d4e516124b137c1745319ef70b60a05a77fa2f
seokjh93 <68522428+seokjh93@users.noreply.github.com>
2020-11-09 18:19:50 +0900

    헤더 자동 반응형 추가 및 스크롤 문제 미해결

--- js/index.js
+++ js/index.js
@@ -3,23 +3,26 @@
         
     $(window).scroll(function(){
         var sct = $(this).scrollTop()
+        var scw = $(this).width()
+        // var videoNear = $('article1').offset().top - $(this).height()/2
 
         console.log($(this).scrollTop())
         if (sct > 0 && sct <= 120) {
-            $('.seok-logo-big-box').css({
-                transform : 'scale(0.8)',
-                transition : 'all 0.5s'
+            $('.seok-logo-big-box img').css({
+                transform : 'scale(0.7)',
+                transformOrigin : 'bottom',
+                transition : 'all 1s'
             })
             $('.article1 video').css({
                 transform : 'scale(0.93)',
-                marginTop : '-70px',
                 transition : 'all 0.3s'
             })
             $('.header-box .seok-logo-box').hide()
         } else if (sct > 120 && sct <= 230) {
-            $('.seok-logo-big-box').css({
+            $('.seok-logo-big-box img').css({
                 transform : 'scale(0.4)',
-                transition : 'all 0.5s'
+                transformOrigin : 'bottom',
+                transition : 'all 1s'
             })
             $('.article1 video').css({
                 transform : 'scale(0.96)',
@@ -27,23 +30,24 @@
             })
             $('.header-box .seok-logo-box').hide()
         } else if (sct > 230 && sct <= 300) {
-            $('.seok-logo-big-box').css({
+            $('.seok-logo-big-box img').css({
                 transform : 'scale(0)',
-                transition : 'all 0.1s'
+                transformOrigin : 'bottom',
+                transition : 'all 0s'
             })
             $('.article1 video').css({
                 transform : 'scale(1)',
                 transition : 'all 0.1s'
             })
-            $('.header-box .seok-logo-box').show(0)
+            $('.header-box .seok-logo-box').delay(500).show()
         } else if (sct === 0) {
-            $('.seok-logo-big-box').css({
+            $('.seok-logo-big-box img').css({
                 transform : 'scale(1)',
+                transformOrigin : 'bottom',
                 transition : 'all 0.5s'
             })
             $('.article1 video').css({
                 transform : 'scale(0.9)',
-                marginTop : '-50px',
                 transition : 'all 0.1s'
             })
             $('.header-box .seok-logo-box').hide()
