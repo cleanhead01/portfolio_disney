3aa7af7d0417fc39e40dfac89416c8e8bd5f7df9
seokjh93 <68522428+seokjh93@users.noreply.github.com>
2020-11-17 18:15:47 +0900

    아티클5 진행중

--- js/index.js
+++ js/index.js
@@ -1,59 +1,5 @@
 (function($){
 
-        
-    // $(window).scroll(function(){
-    //     var sct = $(this).scrollTop()
-    //     var scw = $(this).width()
-    //     // var videoNear = $('article1').offset().top - $(this).height()
-
-    //     console.log($(this).scrollTop())
-    //     if (sct > 10) {
-    //         // $('.seok-logo-big-box img').css({
-    //         //     transform : 'scale(0.7)',
-    //         //     transformOrigin : 'bottom',
-    //         //     transition : 'all 1s'
-    //         // })
-    //         $('.article1 video').css({
-    //             transform : 'scale(0.93)',
-    //             transition : 'all 0.3s'
-    //         })
-    //         // $('.header-box .seok-logo-box').hide()
-    //     } else if (sct > 120 && sct <= 230) {
-    //         // $('.seok-logo-big-box img').css({
-    //         //     transform : 'scale(0.4)',
-    //         //     transformOrigin : 'bottom',
-    //         //     transition : 'all 1s'
-    //         // })
-    //         $('.article1 video').css({
-    //             transform : 'scale(0.96)',
-    //             transition : 'all 0.1s'
-    //         })
-    //         // $('.header-box .seok-logo-box').hide()
-    //     } else if (sct > 230 && sct <= 300) {
-    //         // $('.seok-logo-big-box img').css({
-    //         //     transform : 'scale(0)',
-    //         //     transformOrigin : 'bottom',
-    //         //     transition : 'all 0s'
-    //         // })
-    //         $('.article1 video').css({
-    //             transform : 'scale(1)',
-    //             transition : 'all 0.1s'
-    //         })
-    //         // $('.header-box .seok-logo-box').delay(500).show()
-    //     } else if (sct === 0) {
-    //         // $('.seok-logo-big-box img').css({
-    //         //     transform : 'scale(1)',
-    //         //     transformOrigin : 'bottom',
-    //         //     transition : 'all 0.5s'
-    //         // })
-    //         $('.article1 video').css({
-    //             transform : 'scale(0.9)',
-    //             transition : 'all 0.1s'
-    //         })
-    //         // $('.header-box .seok-logo-box').hide()
-    //     }
-
-    // })
 
     $('.article3 .princess a').on('click', function(){
         $(this).next().slideDown(500)
@@ -63,5 +9,26 @@
     $('.article3 .princess-content-box .princess-content .close-box').on('click', function(){
         $(this).parents('.princess-content-box').hide()
     })
+
+    // 슬라이드 --------------------------------------------------------------------   
+    $('.slide-inner').slick({
+        autoplay : true,
+        autoplaySpeed : 3000,
+        dots : false,
+        speed : 500,
+        centerMode: true,   // centerMode 는 slidesToScroll 값이 1값으로 고정되어 있음
+        centerPadding: '0px',  // 좌우측 끝에 보여질 슬라이드 조각의 너비값
+        slidesToScroll : 1,
+        slidesToShow : 5,
+        pauseOnHover : false,
+        pauseOnDotsHover : false,
+        pauseOnFocus : true,
+        draggable : true,
+        arrows : false,
+        prevArrow : '<button class="prev arr"><i class="fas fa-arrow-left"></i></button>',
+        nextArrow : '<button class="next arr"><i class="fas fa-arrow-right"></i></button>',
+    })
+
+
 
 })(jQuery)
