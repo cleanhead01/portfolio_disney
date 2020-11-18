41912fed94bed7babc17543b168674a833ccb4e6
seokjh93 <68522428+seokjh93@users.noreply.github.com>
2020-11-18 18:09:22 +0900

    아티클5 슬라이드 진행중

--- js/index.js
+++ js/index.js
@@ -11,19 +11,37 @@
     })
 
     // 슬라이드 --------------------------------------------------------------------   
-    $('.slide-inner').slick({
+    $('.slide-inner-txt').slick({
         autoplay : true,
         autoplaySpeed : 3000,
         dots : false,
-        speed : 500,
-        centerMode: true,   // centerMode 는 slidesToScroll 값이 1값으로 고정되어 있음
+        speed : 800,
+        centerMode: false,   // centerMode 는 slidesToScroll 값이 1값으로 고정되어 있음
         centerPadding: '0px',  // 좌우측 끝에 보여질 슬라이드 조각의 너비값
         slidesToScroll : 1,
-        slidesToShow : 5,
+        slidesToShow : 1,
         pauseOnHover : false,
         pauseOnDotsHover : false,
-        pauseOnFocus : true,
-        draggable : true,
+        pauseOnFocus : false,
+        draggable : false,
+        arrows : false,
+        prevArrow : '<button class="prev arr"><i class="fas fa-arrow-left"></i></button>',
+        nextArrow : '<button class="next arr"><i class="fas fa-arrow-right"></i></button>',
+    })
+
+    $('.slide-inner-img').slick({
+        autoplay : true,
+        autoplaySpeed : 3000,
+        dots : false,
+        speed : 800,
+        centerMode: false,   // centerMode 는 slidesToScroll 값이 1값으로 고정되어 있음
+        centerPadding: '0px',  // 좌우측 끝에 보여질 슬라이드 조각의 너비값
+        slidesToScroll : 1,
+        slidesToShow : 4,
+        pauseOnHover : false,
+        pauseOnDotsHover : false,
+        pauseOnFocus : false,
+        draggable : false,
         arrows : false,
         prevArrow : '<button class="prev arr"><i class="fas fa-arrow-left"></i></button>',
         nextArrow : '<button class="next arr"><i class="fas fa-arrow-right"></i></button>',
